ec09f6f97b419e4f7ac24478cd1ae521ea6104e1
rafaelrmou <rafaelrmou2@hotmail.com>
2015-04-26 13:56:01 -0300

    Get Distance - With Calc
    Get Distance - With Calc

--- MapExtend.Maps.Plugin/MapExtend.Maps.Plugin.Forms.Plugin.Abstractions/MapExtend.cs
+++ MapExtend.Maps.Plugin/MapExtend.Maps.Plugin.Forms.Plugin.Abstractions/MapExtend.cs
@@ -68,7 +68,7 @@ namespace Xam.Plugin.MapExtend.Abstractions
         private IEnumerable<Position> Decode(string encodedPoints)
         {
 
-            
+
             if (string.IsNullOrEmpty(encodedPoints))
                 throw new ArgumentNullException("encodedPoints");
 
@@ -183,6 +183,32 @@ namespace Xam.Plugin.MapExtend.Abstractions
             {
                 polilenes.AddRange(lstPos);
             });
+        }
+
+        public double GetDistance(Position From, Position To, char unit = 'K')
+        {
+            double rlat1 = Math.PI * From.Latitude / 180;
+            double rlat2 = Math.PI * To.Latitude / 180;
+            double theta = From.Longitude - To.Longitude;
+            double rtheta = Math.PI * theta / 180;
+            double dist =
+                Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) *
+                Math.Cos(rlat2) * Math.Cos(rtheta);
+            dist = Math.Acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+
+            switch (unit)
+            {
+                case 'K': //Kilometers -> default
+                    return dist * 1.609344;
+                case 'N': //Nautical Miles 
+                    return dist * 0.8684;
+                case 'M': //Miles
+                    return dist;
+            }
+
+            return dist;
         }
 
         /// <summary>
